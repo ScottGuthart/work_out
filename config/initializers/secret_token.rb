1b6ef391778e12419bf2d57344dfaae49d64853c
SEG <SEG@SEGmac.local>
2011-08-25 22:06:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkOut::Application.config.secret_token = '25017639960c7178460d6b61d722a84048ee9c2b95377d100a18a3d1301aee524e5e97dbbc60a2e44d6bd777cf3ca5d1160739a38755f913fed539ca08a12da8'
